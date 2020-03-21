4d085bbb462bc0c4eb35327c475c85bbfba38465
LUDA <arriadnie@gmail.com>
2020-03-21 15:28:48 +0200

    туці туці

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,71 @@
-<footer></footer>
+<footer>
+    <div class="footer-content">
+        <div class="footer-logo">
+            <img src="{{ asset('/image/dorad.png') }}" alt="">
+        </div>
+    </div>
+
+   <div class="footer-content">
+
+       <div class="footer-left">
+           <div class="footer-contacts">
+               <p class="footer-heading">Контакти:</p>
+               <div class="footer-contact-list">
+                   <a type="mail" href="mailto:mail">
+                       <img src="{{ asset('/image/mail.svg') }}" alt="">
+                       <span>test#nfjdkef.co</span>
+                   </a>
+                   <a type="phone" href="tel^tel">
+                       <img src="{{ asset('/image/phone.svg') }}" alt="">
+                       <span>+4354 345 345 34 5</span>
+                   </a>
+
+               </div>
+           </div>
+
+           <div class="footer-contacts">
+               <p class="footer-heading">Соціальні мережі:</p>
+               <div class="footer-socials-list">
+                   <a href="#">
+                       <img src="{{ asset('/image/telegram.svg') }}" alt="">
+                   </a>
+                   <a href="#">
+                       <img src="{{ asset('/image/telegram.svg') }}" alt="">
+                   </a>
+               </div>
+           </div>
+
+
+       </div>
+
+       <div class="footer-right">
+            <div class="footer-menu-item">
+                <a href="#" class="footer-menu-title">Про асоціацію</a>
+
+                <a href="#">Загальна інформація</a>
+                <a href="#">Наша команда</a>
+                <a href="#">Наша діяльність</a>
+            </div>
+
+           <div class="footer-menu-item">
+               <a href="#" class="footer-menu-title">Про асоціацію</a>
+               <a href="#">Загальна інформація</a>
+               <a href="#">Наша діяльність</a>
+           </div>
+           <div class="footer-menu-item">
+               <a href="#" class="footer-menu-title">Дорадництво</a>
+               <a href="#">Загальна інформація</a>
+               <a href="#">Наша діяльність</a>
+               <a href="#">Загальна інформація</a>
+               <a href="#">Наша діяльність</a>
+           </div>
+
+       </div>
+   </div>
+    <div class="copyright">
+        <p>2020 Dorada ©</p>
+        <p>created by Zirael</p>
+    </div>
+
+
+</footer>
